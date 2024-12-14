d1134430c66a4b7c9c798f355f84caf982413f1b
nien11102002 <nguyenduynien11102002@gmail.com>
2024-12-14 22:43:14 +0700

    Done User module
    Api:
    + Edit user info
    + Get info

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -7,16 +7,28 @@ import {
   Param,
   Delete,
   Put,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserInfoDto } from './dto/user-info.dto';
+import { Request } from 'express';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/common/decorators/user.decorator';
+import { TUser } from 'src/common/types/types';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Put('user-info')
-  async changeInfo(@Body() userInfo: UserInfoDto) {
-    return await this.usersService.changeInfo(userInfo);
+  async changeInfo(@Body() userInfo: UserInfoDto, @User() user: TUser) {
+    return await this.usersService.changeInfo(userInfo, user);
+  }
+
+  @Get('get-info')
+  async getInfo(@User() user: TUser) {
+    return await this.usersService.getInfo(user);
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/common/prisma/prisma.service';
 import { UserInfoDto } from './dto/user-info.dto';
+import { TUser } from 'src/common/types/types';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async changeInfo(userInfo: UserInfoDto) {
-    return `Change userInfo`;
+  async changeInfo(userInfo: UserInfoDto, user: TUser) {
+    const { avatar, display_name, full_name, web_link, introduction } =
+      userInfo;
+    const updatedUser = await this.prisma.users.update({
+      where: { id: user.id },
+      data: {
+        avatar: avatar ? avatar : user.avatar,
+        display_name: display_name ? display_name : user.display_name,
+        full_name: full_name ? full_name : user.full_name,
+        web_link: web_link ? web_link : user.web_link,
+        introduction: introduction ? introduction : user.introduction,
+      },
+      select: {
+        id: true,
+        full_name: true,
+        age: true,
+        avatar: true,
+        display_name: true,
+        introduction: true,
+        web_link: true,
+      },
+    });
+
+    return updatedUser;
+  }
+
+  async getInfo(user: TUser) {
+    return user;
   }
 }
